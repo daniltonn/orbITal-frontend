ae69b78a6e634afdab3cb7bf085d53b1da47d997
Jaba <jalejob2015@gmail.com>
2026-03-11 01:06:16 -0500

    feat: Se añade la funcionalidad de listar planetas y registrar equipos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'app/planetas/listar',
+    pathMatch: 'full'
+  },
+  {
+    path: 'app',
+    children: [
+      {
+        path: 'planetas/listar',
+        loadComponent: () =>
+          import('./pages/planetas/listar/listar').then(m => m.ListarComponent)
+      },
+      {
+        path: 'equipos/registrar',
+        loadComponent: () =>
+          import('./pages/equipos/registrar/registrar').then(m => m.RegistrarEquipoComponent)
+      },
+      { path: '**', redirectTo: 'app/planetas/listar' }
+    ]
+  }
+];
