9c1c862ce50a180166c6bd6e01f99effad3c218d
Daniel Felipe Chávez González <danielf.chavezg@konradlorenz.edu.co>
2026-04-15 13:32:35 -0500

    Actualización fondo vista registro-usuario

--- src/app/pages/registro-usuario/registro-usuario/registro-usuario.ts
+++ src/app/pages/registro-usuario/registro-usuario/registro-usuario.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { PanelRegistroUsuario } from "../panel-registro-usuario/panel-registro-usuario";
 import { PanelInfoRol } from "../panel-info-rol/panel-info-rol";
+import { Background } from '../../../shared/backgrounds/login/login';
 
 @Component({
   selector: 'app-registro-usuario-registro-usuario',
   standalone: true,
-  imports: [PanelRegistroUsuario, PanelInfoRol],
+  imports: [PanelRegistroUsuario, PanelInfoRol, Background],
   templateUrl: './registro-usuario.html',
   styleUrl: './registro-usuario.scss',
+
+  
 })
 export class RegistroUsuario {
   rol: string = '';
